0188d1137b014acafe13aa9910af425e2d1cf441
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-09-21 20:42:15 +0300

    post model registered to admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Post
 # Register your models here.
+
+# admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'published', 'status']
+    list_filter = ['author', 'published', 'status']
+    list_editable = ['status']
+    search_fields = ['title', 'body']
+    ordering = ['status', 'published']
+    prepopulated_fields = {'slug': ('title',)}
+    date_hierarchy = 'published'
