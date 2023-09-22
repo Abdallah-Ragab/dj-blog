fe0f6ee25cc4649755830c85e752518f15a1df71
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-09-22 06:50:22 +0300

    list, detail views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+from .models import Post
 # Create your views here.
+
+def post_list(request):
+    posts = Post.publics.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id, status='published')
+    # try:
+    #     post = Post.publics.get(id=id)
+    # except Post.DoesNotExist:
+    #     raise Http404('Post not found')
+    return render(request, 'blog/post_detail.html', {'post': post})
