84b10d032a16d52fe2d5a471f3da1e89220f0786
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-09-22 07:05:41 +0300

    posts detail, list templates

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from .models import Post
 
 def post_list(request):
     posts = Post.publics.all()
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'post_list.html', {'posts': posts})
 
 def post_detail(request, id):
     post = get_object_or_404(Post, id=id, status='published')
@@ -13,4 +13,4 @@ def post_detail(request, id):
     #     post = Post.publics.get(id=id)
     # except Post.DoesNotExist:
     #     raise Http404('Post not found')
-    return render(request, 'blog/post_detail.html', {'post': post})
+    return render(request, 'post_detail.html', {'post': post})
