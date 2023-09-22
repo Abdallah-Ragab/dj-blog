a7437c7ee74764eced3a52330c4d7fd161163feb
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-09-22 08:49:19 +0300

    fix /

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 app_name = 'blog'
 urlpatterns = [
-    path('posts', views.post_list, name='post_list'),
+    path('posts/', views.post_list, name='post_list'),
     path('posts/<int:id>/', views.post_detail, name='post_detail'),
 ]
