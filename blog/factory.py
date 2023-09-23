e95f869db3ee6d9a9fa1ff30d2306c0bcf1af954
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-09-23 06:01:43 +0300

    created post factory

---
+++ blog/factory.py
@@ -0,0 +1,17 @@
+from factory.django import DjangoModelFactory
+from factory.faker import faker
+from factory import Iterator, LazyAttribute, Faker as FakerFactory
+from .models import Post
+from django.utils.text import slugify
+from django.contrib.auth import get_user_model
+
+class PostFactory(DjangoModelFactory):
+    class Meta:
+        model = Post
+
+    title = FakerFactory('sentence', nb_words=4)
+    slug = slugify(title)
+    body = FakerFactory('paragraph', nb_sentences=20, variable_nb_sentences=True)
+    status =FakerFactory('random_element', elements=Post.Status.values)
+    author = Iterator(get_user_model().objects.all())
+
