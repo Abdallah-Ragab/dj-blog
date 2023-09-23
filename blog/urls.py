db8256207eef2deafaa8a7f3fc5743505b34aa07
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-09-23 08:41:18 +0300

    changed post detail url pattern to use unique slug instead of id

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     path('posts/', views.post_list, name='post_list'),
-    path('posts/<int:id>/', views.post_detail, name='post_detail'),
+    path('posts/<slug:slug>/', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -7,11 +7,6 @@ def post_list(request):
     posts = Post.publics.all()
     return render(request, 'post_list.html', {'posts': posts})
 
-def post_detail(request, id):
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLIC)
-    # post = get_object_or_404(Post.publics, id=id)
-    # try:
-    #     post = Post.publics.get(id=id)
-    # except Post.DoesNotExist:
-    #     raise Http404('Post not found')
+def post_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug, status=Post.Status.PUBLIC)
     return render(request, 'post_detail.html', {'post': post})
