884e4f16270f1a245ea02b9ab1a0e93921a736cf
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-09-23 10:23:19 +0300

    created pagination functionality

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,27 @@
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 from .models import Post, PublicPostsManager
+from django.core.paginator import Paginator
+from .utils import get_list_of_pagination_pages
 # Create your views here.
+DEFAULT_PER_PAGE = 9
 
 def post_list(request):
-    posts = Post.publics.all()
-    return render(request, 'post_list.html', {'posts': posts})
+    per_page = int(request.GET.get('per_page', DEFAULT_PER_PAGE))
+    page_number = int(request.GET.get('page', 1))
+    all_posts = Post.publics.all()
+    paginator = Paginator(all_posts, per_page=per_page)
+    posts = paginator.page(page_number)
+    number_of_pages = paginator.num_pages
+    pagination_info = {
+        'current': page_number,
+        'per_page': per_page,
+        'max': number_of_pages,
+        'pages': get_list_of_pagination_pages(page_number, number_of_pages),
+        'next': page_number + 1 if page_number < number_of_pages else None,
+        'prev': page_number - 1 if page_number > 1 else None,
+    }
+    return render(request, 'post_list.html', {'posts': posts, "pagination": pagination_info})
 
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug, status=Post.Status.PUBLIC)
