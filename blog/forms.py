576de21b44b7f6c04706ee085acace623573b640
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-09-26 07:40:31 +0300

    created comment form

--- blog/forms.py
+++ blog/forms.py
@@ -4,3 +4,8 @@ class ShareViaEmailForm(Form):
     sender_name = CharField(max_length=25, required=True)
     sender_email = EmailField(required=True)
     receiver_email = EmailField(required=True)
+
+class CommentForm(Form):
+    name = CharField(max_length=50, required=True)
+    email = EmailField(required=True)
+    content = CharField(max_length=500, required=True)
