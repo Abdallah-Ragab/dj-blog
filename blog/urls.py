e0174415c1e6ec3205a35604d0e95e35dfb394c6
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-09-26 10:37:04 +0300

    switched post list view to class based

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 app_name = 'blog'
 urlpatterns = [
-    path('posts/', views.post_list, name='post_list'),
+    path('posts/', views.PostList.as_view(), name='post_list'),
     path('posts/<slug:slug>/', views.post_detail, name='post_detail'),
     path('posts/<slug:slug>/share/email', views.post_share_email, name='post_share_email'),
+    # path('tags/<slug:slug>/', views.tag_detail, name='tag_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,34 +5,95 @@ from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.conf import settings
-from .models import Comment, Post, PublicPostsManager
+from django.views import View
+from .models import Comment, Post, PublicPostsManager, Tag
 from .utils import get_list_of_pagination_pages
 from .forms import CommentForm, ShareViaEmailForm
 
 DEFAULT_PER_PAGE = 9
 
-def post_list(request):
-    per_page = int(request.GET.get('per_page', DEFAULT_PER_PAGE))
-    page_number = int(request.GET.get('page', 1))
-    all_posts = Post.publics.all()
-    paginator = Paginator(all_posts, per_page=per_page)
-    number_of_pages = paginator.num_pages
-    try:
-        posts = paginator.page(page_number)
-    except EmptyPage:
-        posts = paginator.page(number_of_pages)
-        page_number = number_of_pages
-    except (PageNotAnInteger, ValueError):
-        posts = paginator.page(1)
-    pagination_info = {
-        'current': page_number,
-        'per_page': per_page,
-        'max': number_of_pages,
-        'pages': get_list_of_pagination_pages(page_number, number_of_pages),
-        'next': page_number + 1 if page_number < number_of_pages else None,
-        'prev': page_number - 1 if page_number > 1 else None,
-    }
-    return render(request, 'post_list.html', {'posts': posts, "pagination": pagination_info})
+class PostList(View):
+    DEFAULT_PER_PAGE = DEFAULT_PER_PAGE or 9
+    def get_pagination(self):
+        self.per_page = int(self.request.GET.get('per_page', self.DEFAULT_PER_PAGE))
+        self.page_number = int(self.request.GET.get('page', 1))
+        self.paginator = Paginator(self.queryset, per_page=self.per_page)
+        self.pages = get_list_of_pagination_pages(self.page_number, self.paginator.num_pages)
+        self.next_page = self.page_number + 1 if self.page_number < self.paginator.num_pages else None
+        self.prev_page = self.page_number - 1 if self.page_number > 1 else None
+        self.pagination_info = {
+            'current': self.page_number,
+            'per_page': self.per_page,
+            'max': self.paginator.num_pages,
+            'pages': self.pages,
+            'next': self.next_page,
+            'prev': self.prev_page,
+        }
+
+    def get_paginated_queryset(self):
+        if not hasattr(self, 'paginator') or not hasattr(self, 'page_number'):
+            self.get_pagination()
+        try:
+            queryset = self.paginator.page(self.page_number)
+        except EmptyPage:
+            queryset = self.paginator.page(self.paginator.num_pages)
+            self.page_number = self.paginator.num_pages
+        except (PageNotAnInteger, ValueError):
+            queryset = self.paginator.page(1)
+        return queryset
+
+
+    def get(self, request, *args, **kwargs):
+        self.queryset = Post.publics.all()
+        posts = self.get_paginated_queryset()
+        return render(request, 'post_list.html', {'posts': posts, "pagination": self.pagination_info})
+
+# def tag_detail(request, slug):
+#     per_page = int(request.GET.get('per_page', DEFAULT_PER_PAGE))
+#     page_number = int(request.GET.get('page', 1))
+#     tag = get_object_or_404(Tag, slug=slug)
+#     all_posts = tag.posts.filter(status=Post.Status.PUBLIC)
+#     paginator = Paginator(all_posts, per_page=per_page)
+#     number_of_pages = paginator.num_pages
+#     try:
+#         posts = paginator.page(page_number)
+#     except EmptyPage:
+#         posts = paginator.page(number_of_pages)
+#         page_number = number_of_pages
+#     except (PageNotAnInteger, ValueError):
+#         posts = paginator.page(1)
+#     pagination_info = {
+#         'current': page_number,
+#         'per_page': per_page,
+#         'max': number_of_pages,
+#         'pages': get_list_of_pagination_pages(page_number, number_of_pages),
+#         'next': page_number + 1 if page_number < number_of_pages else None,
+#         'prev': page_number - 1 if page_number > 1 else None,
+#     }
+#     return render(request, 'post_list.html', {'tag': tag, 'posts': posts, "pagination": pagination_info})
+
+# def post_list(request):
+#     per_page = int(request.GET.get('per_page', DEFAULT_PER_PAGE))
+#     page_number = int(request.GET.get('page', 1))
+#     all_posts = Post.publics.all()
+#     paginator = Paginator(all_posts, per_page=per_page)
+#     number_of_pages = paginator.num_pages
+#     try:
+#         posts = paginator.page(page_number)
+#     except EmptyPage:
+#         posts = paginator.page(number_of_pages)
+#         page_number = number_of_pages
+#     except (PageNotAnInteger, ValueError):
+#         posts = paginator.page(1)
+#     pagination_info = {
+#         'current': page_number,
+#         'per_page': per_page,
+#         'max': number_of_pages,
+#         'pages': get_list_of_pagination_pages(page_number, number_of_pages),
+#         'next': page_number + 1 if page_number < number_of_pages else None,
+#         'prev': page_number - 1 if page_number > 1 else None,
+#     }
+#     return render(request, 'post_list.html', {'posts': posts, "pagination": pagination_info})
 
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug, status=Post.Status.PUBLIC)
