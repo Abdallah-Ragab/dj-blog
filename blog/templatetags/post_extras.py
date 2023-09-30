97476f0443bfe053025e9dbd7d758db2fec76d44
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-09-30 18:19:51 +0300

    FIXED Pagination links removes search param from link

--- blog/templatetags/post_extras.py
+++ blog/templatetags/post_extras.py
@@ -10,3 +10,19 @@ def read_time(post_body):
     minutes = words / words_per_minute
     read_time = round(minutes)
     return read_time
+
+# make a custom filter to get the current url and add query params to it
+@register.simple_tag(takes_context=True)
+def url_append_query_params(context, **kwargs):
+    url = context["request"].get_full_path()
+    if "?" in url:
+        url += "&"
+    else:
+        url += "?"
+    for key, value in kwargs.items():
+        # if the param is the last one in the url, don't add the & at the end
+        if key == list(kwargs.keys())[-1]:
+            url += f"{key}={value}"
+        else:
+            url += f"{key}={value}&"
+    return url
