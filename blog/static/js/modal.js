ac4c175703e373764078b0e1d925483237fd59d6
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-09-30 18:45:47 +0300

    email share forms and templates cleaned up & formatted

--- blog/static/js/modal.js
+++ blog/static/js/modal.js
@@ -34,3 +34,46 @@ modalTriggers.forEach((trigger) => {
 modalCloseTriggers.forEach((trigger) => {
   registerModalEvents(trigger)
 });
+
+modalAction = document.querySelector('[data-md-action-trigger = "#md-share-via-email-modal"]')
+    modal = document.querySelector('#md-share-via-email-modal')
+    modalAction.addEventListener('click', e => {
+        e.preventDefault()
+        form = modal.querySelector('form')
+        formData = new FormData(form)
+        endpoint = "{% url 'blog:post_share_email' post.slug %}"
+        fetch(endpoint, {
+            method: 'POST',
+            body: formData
+        }).then(response => response.status).then(status => {
+          if(status == 200){
+            overlay = document.querySelector('#md-share-via-email-modal')
+            overlayBody = overlay.querySelector('.md-overlay-body')
+
+            toggleModal(overlay, overlayBody)
+          }
+          else if (status == 400){
+            overlay = document.querySelector('#md-share-via-email-modal')
+            overlayDangerAlert = overlay.querySelector('.md-massage-danger')
+            overlayDangerAlertBold = overlayDangerAlert.querySelector('span')
+            overlayDangerAlertMessage = overlayDangerAlert.querySelector('p')
+
+
+            overlayDangerAlert.classList.remove('hidden')
+            overlayDangerAlertBold.textContent = 'Failed To Send Email'
+            overlayDangerAlertMessage.textContent = 'Please make sure you have filled all the fields correctly.'
+
+          }
+          else if (status == 500){
+            overlay = document.querySelector('#md-share-via-email-modal')
+            overlayDangerAlert = overlay.querySelector('.md-massage-danger')
+            overlayDangerAlertBold = overlayDangerAlert.querySelector('span')
+            overlayDangerAlertMessage = overlayDangerAlert.querySelector('p')
+
+
+            overlayDangerAlert.classList.remove('hidden')
+            overlayDangerAlertBold.textContent = 'Failed To Send Email'
+            overlayDangerAlertMessage.textContent = 'Something went wrong. Please try again later. If the problem persists, please contact the administrator.'
+          }
+        })
+    })
