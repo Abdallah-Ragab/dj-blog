8a8528e7347151df5b140802412a720313662f00
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-10-02 00:14:28 +0300

    update wsgi.py: add project dir to path

--- base/wsgi.py
+++ base/wsgi.py
@@ -9,6 +9,11 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 
 import os
 
+import sys
+path = '/home/dj-blog/base'
+if path not in sys.path:
+    sys.path.append(path)
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base.settings')
