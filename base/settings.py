9fe59ecd2df94f0323893e0193792169f7281e4a
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-10-02 00:32:10 +0300

    update settings.py : Debug False & Allowed Hosts Set

--- base/settings.py
+++ base/settings.py
@@ -26,9 +26,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j5m!5u4o6&010eq+&((br65j614fgirc=y&ca3d-ks&5=^!&s7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'http://abdallahragab.pythonanywhere.com/',
+]
 
 
 # Application definition
